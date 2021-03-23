535bcaf3ba61bb0f06dff16904f1228015a6fed9
hyejin <dzzd777@naver.com>
2021-03-23 17:07:57 +0900

    [Update]: goods 페이지 마크업

---
+++ page/goods/goods.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="ko">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- favicon -->
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="16x16"
+      href="/openconcert/img/favicon.png"
+    />
+    <!-- awesome font -->
+    <script
+      src="https://kit.fontawesome.com/4d645dbcdc.js"
+      crossorigin="anonymous"
+    ></script>
+    <script src="https://unpkg.com/scrollreveal"></script>
+    <!-- css file -->
+    <link rel="stylesheet" href="/openconcert/css/style.css" />
+    <link rel="stylesheet" href="/openconcert/css/subpage.css" />
+    <link rel="stylesheet" href="/openconcert/css/media.css" />
+    <title>OPEN CONCERT</title>
+  </head>
+  <body>
+    <div class="wrap">
+      <?php include $_SERVER["DOCUMENT_ROOT"]."/openconcert/include/header.php" ?>
+      <section class="goods__section">
+        <div class="sub-title">
+          <h2>GOODS STORE</h2>
+        </div>
+        <div class="goods__container center">
+        </div>
+      </section>
+      <?php include $_SERVER["DOCUMENT_ROOT"]."/openconcert/include/footer.php" ?>
+    </div>
+    <!-- js script -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="/openconcert/js/custom.js"></script>
+    <script src="/openconcert/js/scroll.js"></script>
+
+  </body>
+</html>
