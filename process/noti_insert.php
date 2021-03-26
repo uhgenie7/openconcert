e5be76e6dba7b57fe87d35e8ba770975bf2e5a0b
hyejin <dzzd777@naver.com>
2021-03-26 18:26:41 +0900

    [Update] notice page insert form process

---
+++ process/noti_insert.php
@@ -0,0 +1,31 @@
+<meta charset="UTF-8" />
+<?php
+  $noti_title = nl2br($_REQUEST['noti_title']);
+  $noti_title = addslashes($noti_title);
+  $noti_desc = nl2br($_REQUEST['noti_desc']);
+  $noti_desc = addslashes($noti_desc);
+  $regist_day = date("Y-m-d");
+  $noti_hit = 0;
+
+    //database connect
+    include $_SERVER['DOCUMENT_ROOT'].'/openconcert/process/connect/db_connect.php';
+    $sql="insert into opc_noti(
+    OPC_NOTI_tit,
+    OPC_NOTI_des, 
+    OPC_NOTI_reg,
+    OPC_NOTI_hit) values(
+      '$noti_title',
+      '$noti_desc',
+      '$regist_day',
+      '$noti_hit')";
+
+  mysqli_query($dbConn, $sql);
+
+
+  echo "
+    <script>
+      alert('공지사항이 등록되었습니다.');
+      location.href='/openconcert/page/notice/notice.php';
+    </script>
+    ";
+?>
