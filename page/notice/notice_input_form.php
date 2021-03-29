b51214da2dc3940bc9ea06db2bb14209da93a214
hyejin <dzzd777@naver.com>
2021-03-29 18:30:25 +0900

    [Delete] 네이버 폼 삭제 및 textarea 마크업

--- page/notice/notice_input_form.php
+++ page/notice/notice_input_form.php
@@ -16,13 +16,10 @@
       src="https://kit.fontawesome.com/4d645dbcdc.js"
       crossorigin="anonymous"
     ></script>
-    <script src="https://unpkg.com/scrollreveal"></script>
     <!-- css file -->
     <link rel="stylesheet" href="/openconcert/css/style.css" />
     <link rel="stylesheet" href="/openconcert/css/subpage.css" />
     <link rel="stylesheet" href="/openconcert/css/media.css" />
-    <!-- smart -->
-    <script type="text/javascript" charset="utf-8" src="/openconcert/smarteditor/js/HuskyEZCreator.js"></script>
     <title>OPEN CONCERT</title>
   </head>
   <body>
@@ -34,49 +31,14 @@
         </div>
         <div class="noti__container center">
           <form action="/openconcert/process/noti_insert.php" method="post" name="noti_form">
-            <label for="title" class="title-name">제목</label>
-            <input type="text" placeholder="제목을 입력해주세요." id="title" name="noti_title">
-            <textarea id="noti_desc" class="noti__textarea" name="noti_desc"></textarea>
-            <script type="text/javascript">
-                  var oEditors = [];
-
-                  var sLang = "ko_KR";
-
-                  nhn.husky.EZCreator.createInIFrame({
-                    oAppRef: oEditors,
-                    elPlaceHolder: "noti_desc",
-                    sSkinURI: "/openconcert/smarteditor/SmartEditor2Skin.html",	
-                    htParams : {
-                      bUseToolbar : true,	
-                      bUseVerticalResizer : true,	
-                      bUseModeChanger : true,
-                      fOnBeforeUnload : function(){
-                        //alert("완료!");
-                      },
-                      I18N_LOCALE : sLang
-                    }, //boolean
-                    fOnAppLoad : function(){
-                      oEditors.getById["noti_desc"].exec("PASTE_HTML", [""]);
-                    },
-                    fCreator: "createSEditor2"
-                  });
-
-                  function showHTML() {
-                    var sHTML = oEditors.getById["noti_desc"].getIR();
-                    alert(sHTML);
-                  }
-                    
-                  function submitContents(elClickedObj) {
-                    oEditors.getById["noti_desc"].exec("UPDATE_CONTENTS_FIELD", []);
-                    try {
-                      elClickedObj.form.submit();
-                    } catch(e) {}
-                  }
-          </script>
+            <label for="title" class="noti-title">제목</label>
+            <input type="text" placeholder="제목을 입력해주세요." id="notiTitle" name="noti_title">
+            <label for="noti_desc" class="noti-desc">내용</label>
+            <textarea id="noti_desc" placeholder="내용을 입력해주세요." class="noti__desc" name="noti_desc" spellcheck="false"></textarea>
           </form>
-        </div>
-        <div class="noti__btns center">
-          <button class="noti-write" onclick="submitContents(this);" >글쓰기</button>
+          <div class="noti__btns">
+            <button class="write-btn">글쓰기</button>
+          </div>
         </div>
       </section>
       <?php include $_SERVER["DOCUMENT_ROOT"]."/openconcert/include/footer.php" ?>
@@ -85,7 +47,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="/openconcert/js/custom.js"></script>
     <script>
-        const submitBtn = document.querySelector(".noti-write");
+        const submitBtn = document.querySelector(".write-btn");
         submitBtn.addEventListener('click', function(){
           document.noti_form.submit();
         });
